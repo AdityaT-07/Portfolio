aa89fa7625ac4931e95de2ba89c7e01c3ba9f9cc
Aditya Thakur <adityathakur6076@gmail.com>
2026-02-06 23:54:46 +0530

    defining objects

---
+++ types.js
@@ -0,0 +1,26 @@
+// Project object 
+export const ProjectType = {
+  id: "",
+  title: "",
+  description: "",
+  tech: [],
+  github: "",
+  image: "",
+};
+
+// Skill object structure
+export const SkillType = {
+  name: "",
+  level: 0,
+  icon: "",
+  category: "Language",
+};
+
+// Experience object structure
+export const ExperienceType = {
+  company: "",
+  role: "",
+  duration: "",
+  description: [],
+  skills: [],
+};
